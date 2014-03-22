65a6fa8e6fdab3d1b2f9d0bf59de441b15ae415d
azadeh <azadeh@panthacorp.com>
2014-03-22 15:29:51 +1100

    last comit

--- js/directives.js
+++ js/directives.js
@@ -78,11 +78,16 @@ myApp.directive('googleConnect', ['UserService','UserInteractionService',
                             'userId': 'me'
                         });
                         request.execute(function (resp) {
-                            
+                            console.log(resp);
                             if(resp.image.url>0 && resp.image.url.length>0)
                             {
                             scope.profile.image = resp.image.url;
                             }
+
+                            scope.profile.email = resp.emails[0].value;
+                            
+                            console.log(scope.profile.email)
+
                             scope.profile.displayName=resp.displayName;                          
                             UserService.profile=scope.profile;
                             UserInteractionService.postUserInfo().success(handleSuccess);
